4f1daa1f68324c4e7f6c80900b6382723518fccb
laslopaul <96680549+laslopaul@users.noreply.github.com>
2022-06-05 19:04:32 +0300

    delete update-host.sql

--- update-host.sql
+++
@@ -1,5 +0,0 @@
-# Update wordpress hostname to make it work under /blog endpoint
-# You need to run this script after completing WP installation
-
-UPDATE wp_options SET option_value="http://192.168.56.3/blog/" WHERE option_name="siteurl";
-UPDATE wp_options SET option_value="http://192.168.56.3/blog/" WHERE option_name="home";
